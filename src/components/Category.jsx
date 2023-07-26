62d7e90bc633c95db26dd047672b76df585ee9d6
RyanFahim <rfahim098@gmail.com>
2023-07-26 15:36:53 +0600

    Test

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -13,6 +13,7 @@ const Category = () => {
         {categories.map((item, index) => (
           <div
             key={index}
+            
             className='bg-gray-100 rounded-lg p-4 flex justify-between items-center'
           >
             <h2 className='font-bold sm:text-xl'>{item.name}</h2>
